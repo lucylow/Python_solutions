e567e62f57bac8634c68980c47ff076138562236
Lucy Low <low.lucyy@gmail.com>
2017-04-23 00:09:34 -0700

    Create battlefield.py

---
+++ battlefield.py
@@ -0,0 +1,72 @@
+
+
+
+# Battleship 
+
+
+from random import randint
+
+board = []
+
+# create the board 
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+# begin game 
+print "Let's play Battleship!"
+print_board(board)
+
+# randomize the location of battleship - PC version 
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+#testing
+print ship_row
+print ship_col
+
+
+# user inputs their 2 digit location choice 
+guess_row = int(raw_input("Guess Row:"))
+guess_col = int(raw_input("Guess Col:"))
+
+# check is user choice is equal to battelship location 
+if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sunk my battleship!"
+   
+
+else:
+	# account for size of board 
+    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+        print "Oops, that's not even in the ocean."
+
+    # account for guesses already made 
+    elif(board[guess_row][guess_col] == "X"):
+        print "You guessed that one already."
+
+    # account for maximum number of turns (4) 
+    else:
+        if turn == 3: 
+            print "Game Over" # user only gets 4 turns 
+        else:
+            print "You missed my battleship!"
+            board[guess_row][guess_col] = "X" # replace user guess with X 
+    # Print (turn + 1) here!
+    print_board(board)
+    
+    
+    
+# game allows four guesses before user loses
+for turn in range(4):
+    print "Turn", turn + 1 # current turn user is on 
+
+    
